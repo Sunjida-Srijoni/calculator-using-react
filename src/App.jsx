96402c4c2d3961ff2daf5f40f02170a5a21bc9bd
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-05-14 11:21:21 +0600

    building new design of calculator

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,53 @@ import './App.css'
 import './style.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('')
 
   return (
-    <div className='calculator-grid'>
-      <div className='output'>
-        <div className='previous-operand'>
-        124,675*</div>
-        <div className="current-operand">345,90</div>
-      </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className='span-two'>=</button>
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div>
+            <input type='text'></input>
+          </div>
+          <div>
+            <input type='button' value="AC"></input>
+            <input type='button' value="DE"></input>
+            <input type='button' value="."></input>
+            <input type='button' value="/"></input>
+            
+          </div>
+          <div>
+            <input type='button' value="7"></input>
+            <input type='button' value="8"></input>
+            <input type='button' value="9"></input>
+            <input type='button' value="*"></input>
+            
+          </div>
+          <div>
+            <input type='button' value="4"></input>
+            <input type='button' value="5"></input>
+            <input type='button' value="6"></input>
+            <input type='button' value="+"></input>
+            
+          </div>
 
+          <div>
+            <input type='button' value="1"></input>
+            <input type='button' value="2"></input>
+            <input type='button' value="3"></input>
+            <input type='button' value="-"></input>
+            
+          </div>
+          <div>
+            <input type='button' value="00"></input>
+            <input type='button' value="0"></input>
+            <input type='button' value="="></input>
+           
+            
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
